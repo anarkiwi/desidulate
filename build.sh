9375c74098036bf6ebf14715436053ce1b198151
Josh Bailey <josh@vandervecken.com>
2021-02-20 12:02:15 +1300

    upgrade pip.

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-sudo apt-get update && sudo apt-get install python3-dev && sudo pip3 install -U setuptools && sudo pip3 install -U -r requirements.txt
+sudo apt-get update && \
+  sudo apt-get install python3-dev && \
+  sudo pip3 install -U pip && \
+  sudo pip3 install -U setuptools && \
+  sudo pip3 install -U -r requirements.txt
