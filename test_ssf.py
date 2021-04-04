31c332d3898a62860fa01d8468cadcac833ce81c
Josh Bailey <josh@vandervecken.com>
2021-04-04 20:33:58 +1200

    parser tests.

--- test_ssf.py
+++ test_ssf.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+import os
+import tempfile
 import unittest
 import pandas as pd
-from sidlib import get_sid
+from sidlib import get_sid, get_reg_writes, get_gate_events
 from sidmidi import SidMidiFile
-from ssf import SidSoundFragment
+from ssf import SidSoundFragment, SidSoundFragmentParser
 
 
 class SSFTestCase(unittest.TestCase):
     """Test SSF."""
+
+    def setUp(self):
+        self.tmpdir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.tmpdir.cleanup()
 
     def test_ssf(self):
         sid = get_sid(pal=True)
@@ -20,6 +28,27 @@ class SSFTestCase(unittest.TestCase):
         self.assertEqual(set(s.waveforms.keys()), {'tri'})
         self.assertEqual(s.midi_pitches, (35,))
         self.assertEqual(s.total_duration, 9990435)
+
+    def test_ssf_parser(self):
+        sid = get_sid(True)
+        smf = SidMidiFile(sid, 125)
+        test_log = os.path.join(self.tmpdir.name, 'vicesnd.log')
+        with open(test_log, 'w') as log:
+            log.write('\n'.join((
+                '1 24 15',
+                '1 7 255',
+                '1 8 128',
+                '1 13 255',
+                '100 11 129',
+                '100000 11 0')) + '\n')
+        reg_writes = get_reg_writes(sid, test_log)
+        parser = SidSoundFragmentParser(logfile=None, percussion=True, sid=sid, smf=smf)
+        for voicenum, events in get_gate_events(reg_writes):
+            ssf, first_clock = parser.parse(voicenum, events)
+            self.assertEqual(first_clock, 103)
+            self.assertEqual(ssf.midi_pitches, (95,))
+            self.assertEqual(ssf.total_duration, 98525)
+            self.assertEqual(voicenum, 2)
 
 
 if __name__ == "__main__":
