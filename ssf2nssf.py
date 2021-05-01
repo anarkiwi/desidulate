f36c6db97707682c3f90bdc47e97e04d775811d4
Josh Bailey <josh@vandervecken.com>
2021-05-01 16:05:10 +1200

    denoise.

--- ssf2nssf.py
+++ ssf2nssf.py
@@ -11,9 +11,11 @@ import pandas as pd
 from fileio import out_path
 from ssf import normalize_ssf
 from sidlib import get_sid
+from sidwav import df2samples
 
 parser = argparse.ArgumentParser(description='Normalize [single|multi]_patches.csv')
-parser.add_argument('patchcsv', default='', help='patch CSV to read')
+parser.add_argument('patchcsv', nargs='+', default=[], help='patch CSV(s) to read')
+parser.add_argument('--outcsv', default='', help='patch CSV to write')
 pal_parser = parser.add_mutually_exclusive_group(required=False)
 pal_parser.add_argument('--pal', dest='pal', action='store_true', help='Use PAL clock')
 pal_parser.add_argument('--ntsc', dest='pal', action='store_false', help='Use NTSC clock')
@@ -21,17 +23,29 @@ parser.set_defaults(pal=True)
 args = parser.parse_args()
 
 sid = get_sid(pal=args.pal)
-ssf_dfs = pd.read_csv(args.patchcsv, dtype=pd.Int64Dtype())
-
 new_ssf_dfs = {}
-for _, ssf_df in ssf_dfs.groupby('hashid'):
-    new_ssf = normalize_ssf(ssf_df, sid)
-    hashid = new_ssf['hashid'].max()
-    count = new_ssf['count'].max()
-    if hashid in new_ssf_dfs:
-        new_ssf_dfs[hashid]['count'] += count
-    else:
-        new_ssf_dfs[hashid] = new_ssf
+sample_hashes = {}
+outcsv = args.outcsv
+
+for patchcsv in args.patchcsv:
+    ssf_dfs = pd.read_csv(patchcsv, dtype=pd.Int64Dtype())
+    if not outcsv:
+        outcsv = out_path(patchcsv, 'nssf.txt.xz')
+
+    for _, ssf_df in ssf_dfs.groupby('hashid'):
+        new_ssf = normalize_ssf(ssf_df, sid)
+        hashid = new_ssf['hashid'].max()
+        count = new_ssf['count'].max()
+        if hashid in new_ssf_dfs:
+            new_ssf_dfs[hashid]['count'] += count
+        else:
+            samples_hash = hash(tuple(df2samples(new_ssf, sid)))
+            if samples_hash in sample_hashes:
+                hashid = sample_hashes[samples_hash]
+                new_ssf_dfs[hashid]['count'] += count
+            else:
+                new_ssf_dfs[hashid] = new_ssf
+                sample_hashes[samples_hash] = hashid
 
 ssf_dfs = pd.concat(sorted(new_ssf_dfs.values(), key=lambda x: x['count'].max(), reverse=True))
-ssf_dfs.to_csv(out_path(args.patchcsv, 'nssf.txt.xz'), index=False)
+ssf_dfs.to_csv(outcsv, index=False)
