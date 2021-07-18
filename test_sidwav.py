507e364114a8ef4d1ee51b397f10bf91be387699
Josh Bailey <josh@vandervecken.com>
2021-07-18 19:26:56 +1200

    Test rel changes don't take effect until gateoff.

--- test_sidwav.py
+++ test_sidwav.py
@@ -4,6 +4,7 @@ import os
 import unittest
 import tempfile
 import pandas as pd
+import numpy as np
 import sox
 from sidwav import state2samples, write_wav
 from sidlib import get_sid
@@ -18,6 +19,30 @@ class SidWavTestCase(unittest.TestCase):
     def tearDown(self):
         self.tmpdir.cleanup()
 
+    def _make_wav_df(self, rows):
+        return pd.DataFrame(rows, dtype=pd.UInt64Dtype()).set_index('clock').fillna(method='ffill').astype(pd.UInt64Dtype())
+
+    def test_rel_change_before_gateoff(self):
+        sid = get_sid(pal=True)
+        gateoff = {'hashid': 1, 'clock': 1e6 * 10, 'gate1': 0}
+        end = {'hashid': 1, 'clock': 1e6 * 20, 'freq1': 0}
+        rel = 5
+        df1 = self._make_wav_df([
+            {'hashid': 1, 'clock': 0, 'freq1': 1000, 'sus1': 15, 'gate1': 1, 'tri1': 1, 'vol': 15, 'rel1': rel},
+            gateoff,
+            end,
+        ])
+        raw_samples = state2samples(df1, sid)
+        sid = get_sid(pal=True)
+        df2 = self._make_wav_df([
+            {'hashid': 1, 'clock': 0, 'freq1': 1000, 'sus1': 15, 'gate1': 1, 'tri1': 1, 'vol': 15, 'rel1': rel * 2},
+            {'hashid': 1, 'clock': 1e6 * 1, 'rel1': rel},
+            gateoff,
+            end,
+        ])
+        raw_samples2 = state2samples(df2, sid)
+        self.assertTrue(np.array_equal(raw_samples, raw_samples2))
+
     def test_df2wav(self):
         sid = get_sid(pal=True)
         test_wav = os.path.join(self.tmpdir.name, 'test.wav')
@@ -26,10 +51,9 @@ class SidWavTestCase(unittest.TestCase):
         for i in range(1, 5):
             test_raw_freq = int(i * 2048)
             test_real_freq = sid.real_sid_freq(test_raw_freq)
-            df = pd.DataFrame(
-                [{'hashid': 1, 'count': 1, 'clock': 0, 'freq1': test_raw_freq, 'sus1': 15, 'gate1': 1, 'tri1': 1, 'vol': 15},
-                 {'hashid': 1, 'count': 1, 'clock': 1e6 * 10, 'gate1': 0}], dtype=pd.UInt64Dtype()).set_index('clock')
-            df = df.fillna(method='ffill').astype(pd.UInt64Dtype())
+            df = self._make_wav([
+                {'hashid': 1, 'count': 1, 'clock': 0, 'freq1': test_raw_freq, 'sus1': 15, 'gate1': 1, 'tri1': 1, 'vol': 15},
+                {'hashid': 1, 'count': 1, 'clock': 1e6 * 10, 'gate1': 0}])
             write_wav(test_wav, sid, state2samples(df, sid))
             power_df = pd.DataFrame(transformer.power_spectrum(test_wav), columns=['freq', 'val'])
             val_max = power_df['val'].max()
