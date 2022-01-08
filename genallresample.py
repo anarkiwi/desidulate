6bd9657ab56de56ea769a078aa3b16deb9442851
Josh Bailey <josh@vandervecken.com>
2022-01-08 21:48:09 +0000

    sharded resampling.

--- genallresample.py
+++ genallresample.py
@@ -6,7 +6,7 @@
 
 ## The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-
+import re
 import multiprocessing
 import os
 import pathlib
@@ -14,36 +14,55 @@ import concurrent.futures
 from collections import defaultdict
 import pandas as pd
 
+SSF_GLOB = r'*.resample_ssf.*xz'
+MAXES_RE = re.compile(r'.+\.([^\.]+)\.xz$')
 MAX_WORKERS = multiprocessing.cpu_count()
 
 
-def scrape_resample_dir(resample_dir, resample_dir_dfs):
+def scrape_resample_dir(dir_max, resample_dir, resample_dir_dfs):
     dfs = []
-    cols = None
+    hashids = defaultdict(set)
     for resample_df_file in resample_dir_dfs[resample_dir]:
-        resample_df = pd.read_csv(resample_df_file, dtype=pd.Int64Dtype())
+        if dir_max not in resample_df_file:
+            continue
+        resample_df = pd.read_csv(resample_df_file, dtype=pd.Int64Dtype()).drop(['count'], axis=1)
         if len(resample_df) < 1:
             continue
-        if cols is None:
-            cols = ['source'] + list(resample_df.columns)
-        resample_df.loc[:, ['source']] = resample_df_file[:resample_df_file.find('.')]  # pylint: disable=no-member
+        for hashid in resample_df['hashid'].unique():
+            hashids[int(hashid)].add(resample_df_file[:resample_df_file.find('.')])
         dfs.append(resample_df)
-    return pd.concat(dfs)[cols]
+    return (hashids, pd.concat(dfs).drop_duplicates())
 
 
-def scrape_resample_dfs():
+def scrape_paths():
     current = pathlib.Path(r'./')
     resample_dir_dfs = defaultdict(set)
-    for resample_df_file in current.rglob(r'*.resample_ssf.xz'):
+    maxes = set()
+    for resample_df_path in current.rglob(SSF_GLOB):
+        resample_df_file = str(resample_df_path)
+        maxes_match = MAXES_RE.match(resample_df_file)
+        maxes.add(maxes_match.group(1))
         resample_dir_dfs[os.path.dirname(resample_df_file)].add(str(resample_df_file))
     resample_dirs = resample_dir_dfs.keys()
+    return (maxes, resample_dirs, resample_dir_dfs)
 
+
+def scrape_resample_dfs(dir_max, resample_dirs, resample_dir_dfs):
     with concurrent.futures.ProcessPoolExecutor(max_workers=min(MAX_WORKERS, len(resample_dirs))) as executor:
-        result_futures = map(lambda x: executor.submit(scrape_resample_dir, x, resample_dir_dfs), resample_dirs)
+        result_futures = map(lambda x: executor.submit(scrape_resample_dir, dir_max, x, resample_dir_dfs), resample_dirs)
         results = [future.result() for future in concurrent.futures.as_completed(result_futures)]
 
-    return pd.concat(results)
+    hashids = defaultdict(set)
+    resample_df = pd.concat([i[1] for i in results]).drop_duplicates()
+    for result in results:
+        for hashid, source in result[0].items():
+            hashids[hashid].update(source)
+    hashids_df = pd.DataFrame(hashids.items(), columns=['hashid', 'sources'])
+    resample_df = resample_df.merge(hashids_df, on='hashid')
+    return resample_df
 
 
-df = scrape_resample_dfs()
-df.to_csv('resample_ssf.xz', index=False)
+maxes, resample_dirs, resample_dir_dfs = scrape_paths()
+for dir_max in sorted(maxes):
+    resample_df = scrape_resample_dfs(dir_max, resample_dirs, resample_dir_dfs)
+    resample_df.to_csv('resample_ssf.%s.xz' % dir_max, index=False)
--- resamplessf.py
+++ resamplessf.py
@@ -11,35 +11,49 @@ import sys
 import numpy as np
 import pandas as pd
 from fileio import out_path
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Downsample SSFs')
 parser.add_argument('ssffile', help='SSF file')
 parser.add_argument('--sample_cycles', default=7000, type=int, help='sample interval in CPU cycles')
-parser.add_argument('--max_cycles', default=1e5, type=int, help='max number of CPU cycles')
+parser.add_argument('--max_cycles', default=1e5, type=int, help='include number of CPU cycles')
 
 args = parser.parse_args()
 sample_count = int(args.max_cycles / args.sample_cycles) + 1
+waveform_cols = {'sync1', 'ring1', 'tri1', 'saw1', 'pulse1', 'noise1'}
 sid_cols = {
-    'freq1', 'pwduty1', 'gate1', 'sync1', 'ring1', 'test1', 'tri1', 'saw1', 'pulse1', 'noise1', 'vol',
+    'freq1', 'pwduty1', 'gate1', 'test1', 'vol',
     'fltlo', 'fltband', 'flthi', 'flt1', 'fltext', 'fltres', 'fltcoff',
-    'freq3', 'test3', 'atk1', 'dec1', 'sus1', 'rel1'}
+    'freq3', 'test3', 'atk1', 'dec1', 'sus1', 'rel1'}.union(waveform_cols)
 sample_df = pd.DataFrame([{'clock': i * args.sample_cycles} for i in range(sample_count)], dtype=np.int64)
+sample_max = sample_df['clock'].max()
 
-outfile = out_path(args.ssffile, 'resample_ssf.xz')
 df = pd.read_csv(args.ssffile, dtype=pd.Int64Dtype())
 if len(df) < 1:
     print('ignore empty %s' % args.ssffile)
     sys.exit(0)
 df['clock'] = df['clock'].astype(np.int64)
+df = df[df['clock'] <= sample_max]
 meta_cols = set(df.columns) - sid_cols
-df_raws = []
+df_raws = defaultdict(list)
 for hashid, ssf_df in df.groupby(['hashid']):  # pylint: disable=no-member
+    maxes = set()
+    for waveform in waveform_cols:
+        waveform_max = ssf_df[waveform].max()
+        if pd.notna(waveform_max) and waveform_max > 0:
+            maxes.add(waveform)
+    if not maxes:
+        maxes = {'zero'}
+    maxes = tuple(sorted(maxes))
     resample_df = pd.merge_asof(sample_df, ssf_df).astype(pd.Int64Dtype())
     cols = set(resample_df.columns) - meta_cols
     df_raw = {col: resample_df[col].iat[-1] for col in meta_cols - {'clock', 'frame'}}
     for row in resample_df.itertuples():
         for col in cols:
             df_raw['%s_%u' % (col, row.clock)] = getattr(row, col)
-    df_raws.append(df_raw)
-df = pd.DataFrame(df_raws, dtype=pd.Int64Dtype()).set_index('hashid')
-df.to_csv(outfile)
+    df_raws[maxes].append(df_raw)
+
+for maxes, dfs in df_raws.items():
+    df = pd.DataFrame(dfs, dtype=pd.Int64Dtype()).set_index('hashid')
+    outfile = out_path(args.ssffile, 'resample_ssf.%s.xz' % '-'.join(maxes))
+    df.to_csv(outfile)
