75fc3a0b4acaf609f21335548d966260cf09ca50
Josh Bailey <josh@vandervecken.com>
2022-01-26 14:54:23 +1300

    passes.

--- matchssf.py
+++ matchssf.py
@@ -5,9 +5,6 @@ import copy
 import pandas as pd
 import pyarrow as pa
 
-waveform_order = 'p-n-p'
-dbdir = '/usr/local/datasets/hvsc-76'
-
 
 def read_dfs(waveform_order, dbdir):
     hdf = pd.read_csv('%s/resample_ssf.%s.xz' % (dbdir, waveform_order), dtype=pd.Int64Dtype(), nrows=1)
@@ -20,29 +17,33 @@ def read_dfs(waveform_order, dbdir):
     return (df, match_cols)
 
 
+def fuzzy_match(hashid, df, match_cols):
+    matching_hashids = {hashid}
+    while True:
+        i = len(matching_hashids)
+        hashid_df = df[df.hashid.isin(matching_hashids)]
+        match_df = pd.merge(df, hashid_df, on=match_cols).set_index('hashid_x')
+        noclocks = set(match_df.hashid_noclock_x.unique())
+        matching_hashids.update(set(match_df.index.unique()))
+        matching_hashids.update(set(df[df.hashid_noclock.isin(noclocks)].hashid.unique()))
+        if len(matching_hashids) == i:
+            break
+        print('next')
+    return hashid_df.set_index('hashid')
+
+
 def describe_matches(match_df, waveform_order, dbdir):
     hash_df = pd.read_csv('%s/resample_ssf.hashid.%s.xz' % (dbdir, waveform_order), engine='pyarrow', index_col='hashid')
     xdf = match_df.join(hash_df)
-    print(xdf)
     xdf['sources'] = xdf['sources'].apply(ast.literal_eval)
     for row in xdf.itertuples():
         for source in row.sources:
             print('%s/%s.%d.wav' % (dbdir, source, row.Index))
 
 
+waveform_order = 'p-n-p'
+dbdir = '/usr/local/datasets/hvsc-76'
 hashid = -8980603949025395325
-matching_hashids = {hashid}
 df, match_cols = read_dfs(waveform_order, dbdir)
-
-while True:
-    i = len(matching_hashids)
-    hashid_df = df[df.hashid.isin(matching_hashids)]
-    match_df = pd.merge(df, hashid_df, on=match_cols).set_index('hashid_x')
-    noclocks = set(match_df.hashid_noclock_x.unique())
-    matching_hashids.update(set(match_df.index.unique()))
-    matching_hashids.update(set(df[df.hashid_noclock.isin(noclocks)].hashid.unique()))
-    if len(matching_hashids) == i:
-        break
-    print('next')
-
-describe_matches(df[df.hashid.isin(matching_hashids)].set_index('hashid'), waveform_order, dbdir)
+match_df = fuzzy_match(hashid, df, match_cols)
+describe_matches(match_df, waveform_order, dbdir)
