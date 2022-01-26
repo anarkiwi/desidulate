9dac1648dc5a10ca60c859a79609d48b907ff9a0
Josh Bailey <josh@vandervecken.com>
2022-01-26 14:39:25 +1300

    recurse.

--- matchssf.py
+++ matchssf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import ast
+import copy
 import pandas as pd
 import pyarrow as pa
 
@@ -22,6 +23,7 @@ def read_dfs(waveform_order, dbdir):
 def describe_matches(match_df, waveform_order, dbdir):
     hash_df = pd.read_csv('%s/resample_ssf.hashid.%s.xz' % (dbdir, waveform_order), engine='pyarrow', index_col='hashid')
     xdf = match_df.join(hash_df)
+    print(xdf)
     xdf['sources'] = xdf['sources'].apply(ast.literal_eval)
     for row in xdf.itertuples():
         for source in row.sources:
@@ -32,10 +34,15 @@ hashid = -8980603949025395325
 matching_hashids = {hashid}
 df, match_cols = read_dfs(waveform_order, dbdir)
 
-
 while True:
+    i = len(matching_hashids)
     hashid_df = df[df.hashid.isin(matching_hashids)]
     match_df = pd.merge(df, hashid_df, on=match_cols).set_index('hashid_x')
-    break
+    noclocks = set(match_df.hashid_noclock_x.unique())
+    matching_hashids.update(set(match_df.index.unique()))
+    matching_hashids.update(set(df[df.hashid_noclock.isin(noclocks)].hashid.unique()))
+    if len(matching_hashids) == i:
+        break
+    print('next')
 
-describe_matches(match_df, waveform_order, dbdir)
+describe_matches(df[df.hashid.isin(matching_hashids)].set_index('hashid'), waveform_order, dbdir)
