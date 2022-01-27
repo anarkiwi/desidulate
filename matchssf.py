0f4232c83dbfe3bd5d3aa16219f93addeea5c85c
Josh Bailey <josh@vandervecken.com>
2022-01-28 10:26:51 +1300

    pylint.

--- matchssf.py
+++ matchssf.py
@@ -2,26 +2,20 @@
 
 import argparse
 import ast
-import copy
 from collections import defaultdict
 import pandas as pd
-import pyarrow as pa
-
-sidinfo = None
-
 
 
 def read_dfs(waveform_order, dbdir):
-    #hdf = pd.read_csv('%s/resample_ssf.%s.xz' % (dbdir, waveform_order), dtype=pd.Int64Dtype(), nrows=1)
-    #match_cols = hdf.columns.to_list()
-    #match_cols = [col for col in match_cols if col == 'test1_0' or not col.startswith(('vol', 'atk', 'sus', 'rel', 'flt', 'test'))]
-    #match_cols = [col for col in match_cols if not col.startswith('freq1') or not col[-1].isdigit()]
-    #match_cols = [col for col in match_cols if not col.startswith('pwduty1')]
-    df = pd.read_csv('%s/resample_ssf.%s.xz' % (dbdir, waveform_order), engine='pyarrow', dtype=pd.Int64Dtype(), usecols=['hashid']).fillna(0)
-    #match_cols.remove('hashid')
-    #match_cols.remove('hashid_noclock')
-    #match_cols.remove('gateoff_clock')
-    match_cols = []
+    hdf = pd.read_csv('%s/resample_ssf.%s.xz' % (dbdir, waveform_order), dtype=pd.Int64Dtype(), nrows=1)
+    match_cols = hdf.columns.to_list()
+    match_cols = [col for col in match_cols if col == 'test1_0' or not col.startswith(('vol', 'atk', 'sus', 'rel', 'flt', 'test'))]
+    match_cols = [col for col in match_cols if not col.startswith('freq1') or not col[-1].isdigit()]
+    match_cols = [col for col in match_cols if not col.startswith('pwduty1')]
+    df = pd.read_csv('%s/resample_ssf.%s.xz' % (dbdir, waveform_order), engine='pyarrow', dtype=pd.Int64Dtype()).fillna(0)
+    match_cols.remove('hashid')
+    match_cols.remove('hashid_noclock')
+    match_cols.remove('waveform_last')
     return (df, match_cols)
 
 
@@ -47,13 +41,13 @@ def add_sources(match_df, waveform_order, dbdir):
 
 def describe_matches(sidinfo, match_df, waveform_order, dbdir):
     match_df = add_sources(match_df, waveform_order, dbdir)
-    #sources = set()
-    #for row in match_df.itertuples():
-    #    for source in row.sources:
-    #        print('%s/%s.%d.wav' % (dbdir, source, row.Index))
-    #        sources.add(source)
-    #output = [(row.path, row.ReleasedYear) for row in sidinfo[sidinfo.path.isin(sources)].itertuples()]
-    #print('\n'.join([str(i) for i in sorted(output, key=lambda x: x[1])]))
+    sources = set()
+    for row in match_df.itertuples():
+        for source in row.sources:
+            print('%s/%s.%d.wav' % (dbdir, source, row.Index))
+            sources.add(source)
+    output = [(row.path, row.ReleasedYear) for row in sidinfo[sidinfo.path.isin(sources)].itertuples()]
+    print('\n'.join([str(i) for i in sorted(output, key=lambda x: x[1])]))
 
     sources = set()
     for row in match_df.itertuples():
@@ -74,17 +68,9 @@ def main():
     sidinfo = pd.read_csv('%s/sidinfo.csv' % args.dbdir)
     sidinfo['path'] = sidinfo['path'].apply(lambda x: x[:-4])
     sidinfo['ReleasedYear'] = sidinfo['ReleasedYear'].astype(pd.UInt64Dtype())
-    dfs = []
-
-    for waveform_order in open('hvsc-76/waveforms.txt', 'r').readlines():
-        waveform_order = waveform_order.strip()
-        print(waveform_order)
-        df, match_cols = read_dfs(waveform_order, args.dbdir)
-        #match_df = fuzzy_match(args.hashid, df, match_cols)
-        match_df = df.set_index('hashid')
-        dfs.append(describe_matches(sidinfo, match_df, waveform_order, args.dbdir))
-    df = pd.concat(dfs)
-    df.to_csv('count.csv', index=False)
+    df, match_cols = read_dfs(args.waveform_order, args.dbdir)
+    match_df = fuzzy_match(args.hashid, df, match_cols)
+    describe_matches(sidinfo, match_df, args.waveform_order, args.dbdir)
 
 
 if __name__ == '__main__':
