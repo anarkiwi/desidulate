020e9e40d061b58eefb1abd722db5e1d41fd269f
Josh Bailey <josh@vandervecken.com>
2022-02-04 20:17:16 +0000

    pr_speed.

--- resamplessf.py
+++ resamplessf.py
@@ -16,7 +16,7 @@ from sidlib import df_waveform_order
 
 parser = argparse.ArgumentParser(description='Downsample SSFs to frames')
 parser.add_argument('ssffile', help='SSF file')
-parser.add_argument('--max_frames', default=15, type=int, help='include number of frames')
+parser.add_argument('--max_clock', default=500000, type=int, help='include number of cycles')
 
 args = parser.parse_args()
 waveform_cols = {'sync1', 'ring1', 'tri1', 'saw1', 'pulse1', 'noise1'}
@@ -26,9 +26,6 @@ sid_cols = { # exclude vol, fltext
     'fltlo', 'fltband', 'flthi', 'flt1', 'fltres', 'fltcoff',
     'freq3', 'test3'}.union(waveform_cols).union(adsr_cols)
 big_regs = {'freq1': 8, 'freq3': 8, 'pwduty1': 4, 'fltcoff': 3}
-redundant_adsr_cols = set()
-for col in adsr_cols:
-    redundant_adsr_cols.update({' '.join((col, str(i+1))) for i in range(args.max_frames)})
 
 
 def resample():
@@ -36,19 +33,19 @@ def resample():
     df_raws = defaultdict(list)
     if len(df) < 1:
         return df_raws
-    df = df.drop(['clock', 'vol'], axis=1)
-    df = df[df['vbi_frame'] <= args.max_frames]
+    df = df[df['clock'] <= args.max_clock]
+    df = df.drop(['clock', 'vol', 'vbi_frame', 'rate'], axis=1)
     for col, bits in big_regs.items():
         df[col] = np.left_shift(np.right_shift(df[col], bits), bits)
     meta_cols = set(df.columns) - sid_cols
 
     for _, ssf_df in df.groupby(['hashid']):  # pylint: disable=no-member
-        resample_df = ssf_df.drop_duplicates('vbi_frame', keep='last')
+        resample_df = ssf_df.drop_duplicates('pr_frame', keep='last')
         cols = (set(resample_df.columns) - meta_cols)
-        df_raw = {col: resample_df[col].iat[-1] for col in meta_cols - {'vbi_frame'}}
+        df_raw = {col: resample_df[col].iat[-1] for col in meta_cols - {'pr_frame'}}
         waveforms = df_waveform_order(resample_df)
         for row in resample_df.itertuples():
-            time_cols = {(col, '%s_%u' % (col, row.vbi_frame)) for col in cols} - redundant_adsr_cols
+            time_cols = {(col, '%s_%u' % (col, row.pr_frame)) for col in cols if not (col in adsr_cols and row.pr_frame)}
             df_raw.update({time_col: getattr(row, col) for col, time_col in time_cols})
         for col in big_regs:
             col_raw = resample_df[resample_df[col].notna()][col]
@@ -63,17 +60,18 @@ def resample():
                     df_raw[col_title] = int(col_var)
 
         waveforms = '-'.join(waveforms)
-        df_raws[waveforms].append(df_raw)
+        df_raws[(ssf_df['pr_speed'].iat[0], waveforms)].append(df_raw)
     return df_raws
 
 
 def main():
     df_raws = resample()
-    for waveforms, dfs in df_raws.items():
+    for pr_speed_waveforms, dfs in df_raws.items():
+        pr_speed, waveforms = pr_speed_waveforms
         df = pd.DataFrame(dfs, dtype=pd.Int64Dtype()).set_index('hashid')
         nacols = [col for col in df.columns if df[col].isnull().all() or df[col].max() == 0]
         df = df.drop(nacols, axis=1).drop_duplicates()
-        outfile = out_path(args.ssffile, 'resample_ssf.%s.xz' % waveforms)
+        outfile = out_path(args.ssffile, 'resample_ssf.%u.%s.xz' % (pr_speed, waveforms))
         df.to_csv(outfile)
 
 
