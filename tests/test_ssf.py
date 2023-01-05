a38e76d2e2daa0474864573323c9c131a58e2d84
Josh Bailey <josh@vandervecken.com>
2023-01-05 02:47:53 +0000

    tests.

--- tests/test_ssf.py
+++ tests/test_ssf.py
@@ -4,7 +4,7 @@ import os
 import tempfile
 import unittest
 import pandas as pd
-from desidulate.sidlib import reg2state, state2ssfs
+from desidulate.sidlib import reg2state, state2ssfs, control_labels
 from desidulate.sidmidi import SidMidiFile, MAX_VEL
 from desidulate.sidwrap import get_sid
 from desidulate.ssf import SidSoundFragment, add_freq_notes_df
@@ -20,7 +20,8 @@ class SSFTestCase(unittest.TestCase):
         df = add_freq_notes_df(sid, df)
         df['pr_speed'] = 1
         df['pr_frame'] = df['clock'].floordiv(sid.clockq)
-        df = df.fillna(method='ffill').set_index('clock')
+        df = df.fillna(method='ffill')
+        df = control_labels(df).set_index('clock')
         return SidSoundFragment(percussion=percussion, sid=sid, smf=smf, df=df)
 
     def test_adsr(self):
@@ -38,7 +39,7 @@ class SSFTestCase(unittest.TestCase):
             [{'hashid': 1, 'count': 1, 'clock': 0, 'freq1': 1024, 'pwduty1': 0, 'atk1': 0, 'dec1': 0, 'sus1': 15, 'rel1': 0, 'gate1': 1, 'sync1': 0, 'ring1': 0, 'test1': 0, 'tri1': 1, 'saw1': 0, 'pulse1': 0, 'noise1': 0, 'flt1': 0, 'fltres': 0, 'fltcoff': 0, 'fltlo': 0, 'fltband': 0, 'flthi': 0, 'vol': 15},
              {'hashid': 1, 'count': 1, 'clock': 1e5, 'gate1': 0}], dtype=pd.UInt64Dtype())
         s = self._df2ssf(df, percussion=True)
-        self.assertEqual(s.waveforms, {'tri'})
+        self.assertEqual(s.waveforms, {'t'})
         self.assertEqual(s.midi_pitches, (35,))
         self.assertEqual(s.total_duration, 98280)
         self.assertEqual(s.midi_notes, ((0, 35, s.total_duration, MAX_VEL, 60.134765625),))
@@ -49,7 +50,7 @@ class SSFTestCase(unittest.TestCase):
              {'hashid': 1, 'count': 1, 'clock': 2 * 1e4, 'test1': 0},
              {'hashid': 1, 'count': 1, 'clock': 1e5, 'gate1': 0}], dtype=pd.UInt64Dtype())
         s = self._df2ssf(df, percussion=True)
-        self.assertEqual(s.waveforms, {'tri'})
+        self.assertEqual(s.waveforms, {'Tt', 't'})
         self.assertEqual(s.midi_pitches, (35,))
         self.assertEqual(s.total_duration, 78624)
         self.assertEqual(s.midi_notes, ((20000, 35, s.total_duration, MAX_VEL, 60.134765625),))
@@ -79,6 +80,7 @@ class SSFTestCase(unittest.TestCase):
             ssf_log_df.reset_index(level=0, inplace=True)
             ssf_dfs.reset_index(level=0, inplace=True)
             ssf_dfs = add_freq_notes_df(sid, ssf_dfs)
+            ssf_dfs = control_labels(ssf_dfs)
             ssf = None
             row = None
             for row in ssf_log_df[ssf_log_df.voice > 0].itertuples():
